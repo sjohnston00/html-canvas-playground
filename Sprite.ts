4b62ebe5cded954efc561ec6a28fa8726624453b
Sam Johnston <johnstonsam712@gmail.com>
2023-04-24 08:56:04 +0100

    resolve conflict

--- Sprite.ts
+++ Sprite.ts
@@ -1,33 +1,39 @@
-export default class Shape {
+export default class Sprite {
   private _x: number
   private _y: number
   private _height: number
   private _width: number
   private _dx: number
   private _dy: number
-  private _colour: string
   private _gravity: number
   private _keyState: Map<string, boolean>
+  private _images: Map<string, { img: HTMLImageElement; frames: number }>
+  private _spriteIndex: number = 0
+  private _lastRendered: number = 0
+  private readonly _animationTick = 250
+  private readonly _movingSpeed = 2
+  private readonly _jumpingSpeed = 2
+  private _defaultState = "idle"
 
   constructor(
     x: number,
     y: number,
     height: number,
     width: number,
-    colour: string,
     dx: number,
     dy: number,
+    img: Map<string, { img: HTMLImageElement; frames: number }>,
     gravity: number = 0.5
   ) {
     this._x = x
     this._y = y
     this._height = height
     this._width = width
-    this._colour = colour
     this._dx = dx
     this._dy = dy
-    this._gravity = gravity
+    this._images = img
     this._keyState = new Map<string, boolean>()
+    this._gravity = gravity
   }
 
   public draw(ctx: CanvasRenderingContext2D): void {
@@ -40,23 +46,63 @@ export default class Shape {
     // gradient.addColorStop(0, this._colour)
     // gradient.addColorStop(0.5, this._colour)
     // gradient.addColorStop(1, 'black')
-    ctx.fillStyle = this._colour
-    ctx.fillRect(this._x, this._y, this._width, this._height)
+
+    if (this._keyState.has(" ")) {
+      this._defaultState = "jumping"
+    } else if (this._keyState.has("ArrowLeft")) {
+      this._defaultState = "run"
+    } else if (this._keyState.has("ArrowRight")) {
+      this._defaultState = "run"
+    } else {
+      this._defaultState = "idle"
+    }
+
+    const img1 = this._images.get(this._defaultState)
+    if (!img1) throw new Error("image not found")
+    const { img, frames } = img1
+
+    let spriteHeight = img.height
+    let spriteWidth = img.width / frames
+    ctx.drawImage(
+      img,
+      spriteWidth * this._spriteIndex,
+      0,
+      spriteWidth,
+      spriteHeight,
+      this._x,
+      ctx.canvas.height - 100 - spriteHeight,
+      spriteWidth,
+      spriteHeight
+    )
   }
 
   public update(ctx: CanvasRenderingContext2D): void {
+    if (Date.now() - this._lastRendered > this._animationTick) {
+      this._lastRendered = Date.now()
+      if (
+        this._spriteIndex ===
+        (this._images.get(this._defaultState)?.frames || 0) - 1
+      ) {
+        this._spriteIndex = 0
+      } else {
+        this._spriteIndex++
+      }
+    }
     const innerHeight = ctx.canvas.height
     const innerWidth = ctx.canvas.width
+
+    console.log({ controls: this._keyState })
+
     // console .log(this._keyState)
 
     if (this._keyState.has("ArrowRight")) {
-      this._dx = 10
+      this._dx = this._movingSpeed
     } else if (this._keyState.has("ArrowLeft")) {
-      this._dx = -10
+      this._dx = -this._movingSpeed
     }
 
-    if (this._keyState.has(" ")) {
-      this._dy = -10
+    if (this.IsJumping) {
+      this._dy = -this._jumpingSpeed
     }
 
     if (this._x + this._width > innerWidth) {
@@ -123,12 +169,7 @@ export default class Shape {
   public set dy(value: number) {
     this._dy = value
   }
-  public get colour(): string {
-    return this._colour
-  }
-  public set colour(value: string) {
-    this._colour = value
-  }
+
   public get gravity(): number {
     return this._gravity
   }
@@ -143,5 +184,25 @@ export default class Shape {
   }
   public removeKeyState(value: string) {
     this._keyState.delete(value)
+  }
+
+  public get images(): Map<string, { img: HTMLImageElement; frames: number }> {
+    return this._images
+  }
+  public set images(
+    value: Map<string, { img: HTMLImageElement; frames: number }>
+  ) {
+    this._images = value
+  }
+
+  public get IsJumping(): boolean {
+    return !!this._keyState.get(" ")
+  }
+  public set IsJumping(value: boolean) {
+    if (value) {
+      this._keyState.set(" ", true)
+    } else {
+      this._keyState.delete(" ")
+    }
   }
 }
--- js/index.ts
+++ js/index.ts
@@ -1,16 +1,34 @@
-import Shape from './Shape.js'
-const canvas = document.getElementById('canvas') as HTMLCanvasElement
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+import Shape from "./Shape.js"
+import Sprite from "../Sprite.js"
+const canvas = document.getElementById("canvas") as HTMLCanvasElement
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
-const amount = document.getElementById('amount') as HTMLInputElement
-const amountSpan = document.getElementById('amount-span') as HTMLSpanElement
-const speed = document.getElementById('speed') as HTMLInputElement
-const speedSpan = document.getElementById('speed-span') as HTMLSpanElement
+ctx.imageSmoothingEnabled = true
+ctx.imageSmoothingQuality = "high"
 
-let shapeSpeed = speed?.valueAsNumber || 1
+const amount = document.getElementById("amount") as HTMLInputElement
+const amountSpan = document.getElementById("amount-span") as HTMLSpanElement
 let shapesAmount = amount?.valueAsNumber || 1
 
-amount?.addEventListener('input', (e) => {
+const idle = new Image()
+idle.src = "/Punk/Punk_idle.png"
+
+const jumping = new Image()
+jumping.src = "/Punk/Punk_jump.png"
+
+const running = new Image()
+running.src = "/Punk/Punk_run.png"
+
+const spriteImages = new Map<
+  string,
+  { img: HTMLImageElement; frames: number }
+>()
+spriteImages.set("idle", { img: idle, frames: 4 })
+spriteImages.set("jumping", { img: jumping, frames: 4 })
+spriteImages.set("run", { img: running, frames: 6 })
+const sprite = new Sprite(0, 0, 48, 48, 0, 0, spriteImages, 4)
+
+amount?.addEventListener("input", (e) => {
   //adding and remove items rather than re-rendering
   // if (amount.valueAsNumber > shapesAmount) {
   //   const { x, y, height, width, dx, dy } = generateRandomShape()
@@ -23,7 +41,11 @@ amount?.addEventListener('input', (e) => {
   init()
 })
 
-speed?.addEventListener('input', (e) => {
+const speed = document.getElementById("speed") as HTMLInputElement
+const speedSpan = document.getElementById("speed-span") as HTMLSpanElement
+let shapeSpeed = speed?.valueAsNumber || 1
+
+speed?.addEventListener("input", (e) => {
   shapeSpeed = speed.valueAsNumber || 1
   speedSpan.textContent = shapeSpeed.toString()
 
@@ -37,10 +59,7 @@ speed?.addEventListener('input', (e) => {
 let innerHeight = window.innerHeight
 let innerWidth = window.innerWidth
 
-canvas.height = innerHeight
-canvas.width = innerWidth
-
-window.addEventListener('resize', (e) => {
+window.addEventListener("resize", (e) => {
   canvas.height = window.innerHeight
   canvas.width = window.innerWidth
 
@@ -50,34 +69,23 @@ window.addEventListener('resize', (e) => {
   init()
 })
 
-window.addEventListener('keydown', (e) => {
-  square.addKeyState(e.key)
-  if (e.key === ' ') {
-    console.log('spacebar')
+window.addEventListener("keydown", (e) => {
+  sprite.addKeyState(e.key)
+  if (e.key === " ") {
+    sprite.IsJumping = true
   }
-  // switch (e.key) {
-  //   case 'ArrowRight':
-  //     square.dx = 10
-  //     break
-  //   case 'ArrowLeft':
-  //     square.dx = -10
-  //     break
-  //   case ' ':
-  //     square.dy = -10
-  //   default:
-  //     break
-  // }
 })
 
-window.addEventListener('touchstart', (e) => {
-  square.addKeyState(' ')
+window.addEventListener("touchstart", (e) => {
+  square.addKeyState(" ")
 })
-window.addEventListener('touchend', (e) => {
-  square.removeKeyState(' ')
+window.addEventListener("touchend", (e) => {
+  square.removeKeyState(" ")
 })
-window.addEventListener('keyup', (e) => {
-  square.removeKeyState(e.key)
-  square.dx = 0
+
+window.addEventListener("keyup", (e) => {
+  sprite.removeKeyState(e.key)
+  sprite.dx = 0
 })
 
 function findClosestShape(x: number, y: number): Shape | null {
@@ -96,7 +104,7 @@ function findClosestShape(x: number, y: number): Shape | null {
 
 let shapes = new Map<string, Shape>()
 
-const square = new Shape(10, 1, 40, 40, 'red', 0, 0)
+const square = new Shape(10, 1, 40, 40, "red", 0, 0)
 
 function init() {
   shapes = new Map<string, Shape>()
@@ -125,59 +133,54 @@ function generateRandomMovingShape() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-
+  sprite.update(ctx)
   shapes.forEach((s) => {
     s.update(ctx)
   })
-  square.update(ctx)
-
-  if (square.keyState.has(' ')) {
-    const shape = findClosestShape(square.x, square.y)
+  // square.update(ctx)
+  if (sprite.IsJumping) {
     ctx.beginPath()
-    ctx.moveTo(square.x + square.width / 2, square.y + square.height / 2)
-    ctx.lineTo(
-      (shape?.x || 0) + (shape?.width || 0) / 2,
-      (shape?.y || 0) + (shape?.height || 0) / 2
-    )
-    ctx.strokeStyle = 'black'
+    ctx.moveTo(sprite.x + sprite.width / 2, sprite.y + sprite.height / 2)
+    ctx.lineTo(200, 200)
+    ctx.strokeStyle = "black"
     ctx.stroke()
   }
 
   // console.log(shapes)
 
   //bottom
-  ctx.strokeStyle = 'black'
-  ctx.strokeRect(0, innerHeight - 100, innerWidth, 100)
+  ctx.strokeStyle = "black"
+  ctx.strokeRect(-1, innerHeight - 100, innerWidth + 1, 100)
 
   //circle 1
-  // ctx.beginPath()
-  // ctx.arc(100, 100, 20, 0, 2 * Math.PI)
-  // ctx.fillStyle = 'black'
-  // ctx.fill()
+  ctx.beginPath()
+  ctx.arc(100, 100, 20, 0, 2 * Math.PI)
+  ctx.fillStyle = "black"
+  ctx.fill()
 
-  // //circle 2
-  // ctx.beginPath()
-  // ctx.arc(200, 200, 20, 0, 2 * Math.PI)
-  // ctx.fillStyle = 'black'
-  // ctx.fill()
+  //circle 2
+  ctx.beginPath()
+  ctx.arc(200, 200, 20, 0, 2 * Math.PI)
+  ctx.fillStyle = "black"
+  ctx.fill()
 
-  // //circle 3
-  // ctx.beginPath()
-  // ctx.arc(300, 100, 20, 0, 2 * Math.PI)
-  // ctx.fillStyle = 'black'
-  // ctx.fill()
+  //circle 3
+  ctx.beginPath()
+  ctx.arc(300, 100, 20, 0, 2 * Math.PI)
+  ctx.fillStyle = "black"
+  ctx.fill()
 
-  // //circle 4
-  // ctx.beginPath()
-  // ctx.arc(400, 200, 20, 0, 2 * Math.PI)
-  // ctx.fillStyle = 'black'
-  // ctx.fill()
+  //circle 4
+  ctx.beginPath()
+  ctx.arc(400, 200, 20, 0, 2 * Math.PI)
+  ctx.fillStyle = "black"
+  ctx.fill()
 
-  // //circle 5
-  // ctx.beginPath()
-  // ctx.arc(500, 100, 20, 0, 2 * Math.PI)
-  // ctx.fillStyle = 'black'
-  // ctx.fill()
+  //circle 5
+  ctx.beginPath()
+  ctx.arc(500, 100, 20, 0, 2 * Math.PI)
+  ctx.fillStyle = "black"
+  ctx.fill()
   requestAnimationFrame(draw)
 }
 
